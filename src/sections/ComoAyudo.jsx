36f79813dd96e09865ebe807e4db65f9cf24ab6c
Adao Iturrusgarai <adaoiturrusgarai@Adaos-MacBook-Air.local>
2025-07-05 20:44:33 -0300

    aaaa

--- src/sections/ComoAyudo.jsx
+++ src/sections/ComoAyudo.jsx
@@ -7,7 +7,7 @@ export default function ComoAyudo() {
       <p className="max-w-xl mx-auto mb-8">Si estás atravesando un momento difícil y no sabés por dónde empezar, puedo ayudarte a entender tus derechos y tus opciones para que tomes decisiones con tranquilidad.</p></div>
      
         <div className="sm:justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl mx-auto">
-        <div className="flex items-center gap-3 justify-start"><Users className="font-montserrat" /> Alimentos y régimen de comunicación.</div>
+        <div className="flex items-center gap-3"><Users className="font-montserrat" /> Alimentos y régimen de comunicación.</div>
         <div className="flex items-center gap-3"><HeartHandshake className="text-primary" /> Divorcio y división de bienes.</div>
         <div className="flex items-center gap-3"><Baby className="text-primary" /> Adopción por integración.</div>
         <div className="flex items-center gap-3"><Landmark className="text-primary" /> Declaratoria de herederos y sucesiones.</div>
--- src/sections/Funcionamiento.jsx
+++ src/sections/Funcionamiento.jsx
@@ -2,14 +2,18 @@ import { Check, Video, Clock, FileText } from 'lucide-react';
 
 export default function Funcionamiento() {
   return (
-    <section className="py-8 sm:py-16 px-4 bg-[#e7c9bc] ">
-      <h2 className="text-2xl md:text-4xl text-center font-montserrat mb-6">¿Cómo funciona la consulta?</h2>
-      <div className="space-y-4 max-w-xl mx-auto">
-        <div className="flex items-center gap-3 justify-center"><Video className="text-primary w-6 h-6" /> Modalidad: 100% online (videollamada).</div>
-        <div className="flex items-center gap-3 justify-center"><Clock className="text-primary w-6 h-6" /> Duración: 45 minutos, máximo 1 hora.</div>
-        <div className="flex items-center gap-3 justify-center"><FileText className="text-primary w-6 h-6" /> Incluye: escucha, aclaración de dudas y posibles pasos a seguir.</div>
-        <div className="flex items-center gap-3 justify-center"><Check className="text-primary w-6 h-6" /> Horarios disponibles: mañana (8:30 a 12:30) y tarde (16:30 a 19:30).</div>
+    <section className="py-8 sm:py-16 px-4 bg-[#e7c9bc]">
+        <div className='text-center'> <h2 className="text-2xl md:text-4xl font-montserrat mb-6">¿Cómo funciona la consulta?</h2></div>
+     
+        <div className="sm:justify-items-center grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl mx-auto">
+        <div className="flex items-center gap-3"><Video className="font-montserrat" /> Modalidad: 100% online (videollamada).</div>
+        <div className="flex items-center gap-3"><Clock className="text-primary" /> Duración: 45 minutos, máximo 1 hora.</div>
+        <div className="flex items-center gap-3"><FileText className="text-primary" /> Incluye: escucha, aclaración de dudas y posibles pasos a seguir.</div>
+        <div className="flex items-center gap-3"><Check className="text-primary" /> Horarios disponibles: mañana (8:30 a 12:30) y tarde (16:30 a 19:30).</div>
+       
       </div>
+      
     </section>
   );
 }
+
